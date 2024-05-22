87ce0141c3cc7990dcbc0fab7610e4d848286b41
Mohan-Zhang-u <mohan.zhang.mz@gmail.com>
2024-05-22 17:02:22 -0400

    changed

--- basic_demo/cli_demo.py
+++ basic_demo/cli_demo.py
@@ -10,7 +10,8 @@ import torch
 from PIL import Image
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-MODEL_PATH = "/share/home/zyx/Models/cogvlm2-llama3-chat-19B"
+# MODEL_PATH = "/share/home/zyx/Models/cogvlm2-llama3-chat-19B"
+MODEL_PATH = "/home/mohan/Projects/CogVLM2/models/THUDM/cogvlm2-llama3-chat-19B"
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 TORCH_TYPE = torch.bfloat16 if torch.cuda.is_available() and torch.cuda.get_device_capability()[
     0] >= 8 else torch.float16
